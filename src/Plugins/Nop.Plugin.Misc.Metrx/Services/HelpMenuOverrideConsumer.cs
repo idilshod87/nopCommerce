1ebdf5e17dbdafd2a28779f619f1469f9e4e7526
dilshod <ismatov.dilshod@gmail.com>
2026-01-12 18:23:00 +0500

    feat: Refactor admin layout and menu structure, remove Help menu node, and enhance customer list view

---
+++ src/Plugins/Nop.Plugin.Misc.Metrx/Services/HelpMenuOverrideConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Nop.Services.Events;
+using Nop.Web.Framework.Events;
+
+namespace Nop.Plugin.Misc.Metrx.Services;
+
+/// <summary>
+/// Removes the default "Help" menu node from the admin navigation.
+/// </summary>
+public class HelpMenuOverrideConsumer : IConsumer<AdminMenuCreatedEvent>
+{
+    public Task HandleEventAsync(AdminMenuCreatedEvent eventMessage)
+    {
+        if (eventMessage?.RootMenuItem?.ChildNodes == null)
+            return Task.CompletedTask;
+
+        var helpNode = eventMessage.RootMenuItem.ChildNodes
+            .FirstOrDefault(node => string.Equals(node.SystemName, "Help", StringComparison.InvariantCultureIgnoreCase));
+
+        if (helpNode != null)
+            eventMessage.RootMenuItem.ChildNodes.Remove(helpNode);
+
+        return Task.CompletedTask;
+    }
+}
