714f2fe3e536a036d7d86e38f1f744ae44435663
dilshod <ismatov.dilshod@gmail.com>
2026-01-27 14:13:37 +0500

    Increase token expiry default; change expiry to minutes
    Increased the default access token expiration from 1 to 2 (now interpreted as minutes, not days). Updated JwtTokenService to calculate token expiration in minutes instead of days.

--- src/Plugins/Nop.Plugin.Api/Services/JwtTokenService.cs
+++ src/Plugins/Nop.Plugin.Api/Services/JwtTokenService.cs
@@ -31,7 +31,7 @@ public class JwtTokenService : IJwtTokenService
     public JwtTokenResult GenerateToken(Customer customer)
     {
         var currentTime = DateTimeOffset.Now;
-        var expirationTime = currentTime.AddDays(GetTokenExpiryInDays());
+        var expirationTime = currentTime.AddMinutes(GetTokenExpiryInDays());
 
         var claims = new List<Claim>
         {
